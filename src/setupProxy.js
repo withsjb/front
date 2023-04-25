c71f2fa2057d56462f8a24b29e05b5128dec38c9
withsjb <withsjb1@gmail.com>
2023-04-25 10:51:51 +0900

    proxy11

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://port-0-back-17xqnr2llgv1tyhz.sel3.cloudtype.app/api",
+      target: "https://port-0-back-17xqnr2llgv1tyhz.sel3.cloudtype.app",
       changeOrigin: true,
     })
   );
