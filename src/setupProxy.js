44e8c2224e744d1b44f58763b389b7581ab41b7d
withsjb <withsjb1@gmail.com>
2023-04-25 12:25:04 +0900

    setpro

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/",
-    createProxyMiddleware({
-      target: "https://port-0-back-17xqnr2llgv1tyhz.sel3.cloudtype.app",
+    createProxyMiddleware("/api", {
+      target: "https://port-0-back-17xqnr2llgv1tyhz.sel3.cloudtype.app/",
       changeOrigin: true,
     })
   );
