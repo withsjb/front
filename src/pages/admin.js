573f55c0d6eb913cceb759b77613022f597a1fb9
withsjb <withsjb1@gmail.com>
2023-06-08 20:02:03 +0900

    divupdate

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -63,86 +63,87 @@ const AddQuestion = () => {
 
   return (
     <>
-      <div className={Styles.adminbody}></div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="question">질문:</label>
-          <input
-            type="text"
-            id="question"
-            value={question}
-            onChange={(event) => setQuestion(event.target.value)}
-          />
-        </div>
+      <div className={Styles.adminbody}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="question">질문:</label>
+            <input
+              type="text"
+              id="question"
+              value={question}
+              onChange={(event) => setQuestion(event.target.value)}
+            />
+          </div>
 
-        <div>
-          <label htmlFor="text">텍스트:</label>
-          <input
-            type="text"
-            id="text"
-            value={text}
-            onChange={(event) => setText(event.target.value)}
-          />
-        </div>
+          <div>
+            <label htmlFor="text">텍스트:</label>
+            <input
+              type="text"
+              id="text"
+              value={text}
+              onChange={(event) => setText(event.target.value)}
+            />
+          </div>
 
-        <div>
-          <label htmlFor="option1">선택지 1:</label>
-          <input
-            type="text"
-            id="option1"
-            value={options[0]}
-            onChange={(event) => handleOptionChange(event, 0)}
-          />
-        </div>
-        <div>
-          <label htmlFor="option2">선택지 2:</label>
-          <input
-            type="text"
-            id="option2"
-            value={options[1]}
-            onChange={(event) => handleOptionChange(event, 1)}
-          />
-        </div>
-        <div>
-          <label htmlFor="option3">선택지 3:</label>
-          <input
-            type="text"
-            id="option3"
-            value={options[2]}
-            onChange={(event) => handleOptionChange(event, 2)}
-          />
-        </div>
-        <div>
-          <label htmlFor="option4">선택지 4:</label>
-          <input
-            type="text"
-            id="option4"
-            value={options[3]}
-            onChange={(event) => handleOptionChange(event, 3)}
-          />
-        </div>
-        <div>
-          <label htmlFor="answer">정답:</label>
-          <select
-            id="answer"
-            value={answer}
-            onChange={(event) => setAnswer(event.target.value)}
-          >
-            <option value={0}>선택지 1</option>
-            <option value={1}>선택지 2</option>
-            <option value={2}>선택지 3</option>
-            <option value={3}>선택지 4</option>
-          </select>
-        </div>
+          <div>
+            <label htmlFor="option1">선택지 1:</label>
+            <input
+              type="text"
+              id="option1"
+              value={options[0]}
+              onChange={(event) => handleOptionChange(event, 0)}
+            />
+          </div>
+          <div>
+            <label htmlFor="option2">선택지 2:</label>
+            <input
+              type="text"
+              id="option2"
+              value={options[1]}
+              onChange={(event) => handleOptionChange(event, 1)}
+            />
+          </div>
+          <div>
+            <label htmlFor="option3">선택지 3:</label>
+            <input
+              type="text"
+              id="option3"
+              value={options[2]}
+              onChange={(event) => handleOptionChange(event, 2)}
+            />
+          </div>
+          <div>
+            <label htmlFor="option4">선택지 4:</label>
+            <input
+              type="text"
+              id="option4"
+              value={options[3]}
+              onChange={(event) => handleOptionChange(event, 3)}
+            />
+          </div>
+          <div>
+            <label htmlFor="answer">정답:</label>
+            <select
+              id="answer"
+              value={answer}
+              onChange={(event) => setAnswer(event.target.value)}
+            >
+              <option value={0}>선택지 1</option>
+              <option value={1}>선택지 2</option>
+              <option value={2}>선택지 3</option>
+              <option value={3}>선택지 4</option>
+            </select>
+          </div>
 
-        <div>
-          <label htmlFor="photo">사진:</label>
-          <input type="file" id="photo" onChange={handleFileChange} />
-        </div>
+          <div>
+            <label htmlFor="photo">사진:</label>
+            <input type="file" id="photo" onChange={handleFileChange} />
+          </div>
 
-        <button type="submit">추가하기</button>
-      </form>
-      <Getquestion />
+          <button type="submit">추가하기</button>
+        </form>
+        <Getquestion />
+      </div>
     </>
   );
 };
