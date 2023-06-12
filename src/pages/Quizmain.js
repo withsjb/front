8e89a71c13aa52d67256d0b2b97aac3657b7d692
withsjb <withsjb1@gmail.com>
2023-06-13 07:29:28 +0900

    appprovider

--- src/pages/Quizmain.js
+++ src/pages/Quizmain.js
@@ -3,8 +3,6 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { setUserId } from "../redux/result_reducer";
 import Styles from "../styles/Quizmain.module.css";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 
 export default function Main() {
   const inputRef = useRef(null);
@@ -17,35 +15,33 @@ export default function Main() {
   }
 
   return (
-    <Provider store={store}>
-      <div className={Styles.container}>
-        <h1 className={Styles.title}>Quiz Application</h1>
+    <div className={Styles.container}>
+      <h1 className={Styles.title}>Quiz Application</h1>
 
-        <ol>
-          <li>You will be asked 10 questions one after another.</li>
-          <li>10 points is awarded for the correct answer.</li>
-          <li>
-            Each question has three options. You can choose only one options.
-          </li>
-          <li>You can review and change answers before the quiz finish.</li>
-          <li>The result will be declared at the end of the quiz.</li>
-        </ol>
+      <ol>
+        <li>You will be asked 10 questions one after another.</li>
+        <li>10 points is awarded for the correct answer.</li>
+        <li>
+          Each question has three options. You can choose only one options.
+        </li>
+        <li>You can review and change answers before the quiz finish.</li>
+        <li>The result will be declared at the end of the quiz.</li>
+      </ol>
 
-        <form className={Styles.form} id="form">
-          <input
-            ref={inputRef}
-            className={Styles.userid}
-            type="text"
-            placeholder="Username*"
-          />
-        </form>
+      <form className={Styles.form} id="form">
+        <input
+          ref={inputRef}
+          className={Styles.userid}
+          type="text"
+          placeholder="Username*"
+        />
+      </form>
 
-        <div className={Styles.start}>
-          <Link className={Styles.btn} to={"/quiz"} onClick={startQuiz}>
-            Start Quiz
-          </Link>
-        </div>
+      <div className={Styles.start}>
+        <Link className={Styles.btn} to={"/quiz"} onClick={startQuiz}>
+          Start Quiz
+        </Link>
       </div>
-    </Provider>
+    </div>
   );
 }
