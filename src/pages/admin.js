9e854981a8cc7daeed27976c8c18724a44c767bf
withsjb <withsjb1@gmail.com>
2023-06-20 05:31:40 +0900

    fulladd

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { AiFillPlusCircle } from "react-icons/ai";
 import Getquestion from "../component/admin/getquestion";
-import styled from "styled-components";
 import Styles from "../styles/admin.module.css";
 
 const AddQuestion = () => {
@@ -31,9 +30,7 @@ const AddQuestion = () => {
 
       const formData = new FormData();
       formData.append("questions", JSON.stringify(questionData));
-      if (photo !== null) {
-        formData.append("photo", photo);
-      }
+      formData.append("photo", photo || null);
 
       const response = await axios.post("/api/quiz", formData, {
         headers: {
@@ -70,87 +67,94 @@ const AddQuestion = () => {
 
   return (
     <>
-      <div className={Styles.adminbody}>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="question">질문:</label>
-            <input
-              type="text"
-              id="question"
-              value={question}
-              onChange={(event) => setQuestion(event.target.value)}
-            />
-          </div>
+      <form className={Styles.inputform} onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="question"> 질문 입력 : </label>
+          <input
+            type="text"
+            id="question"
+            value={question}
+            placeholder=" 질문을 입력하세요."
+            onChange={(event) => setQuestion(event.target.value)}
+          />
+        </div>
 
+        <div>
+          <label htmlFor="text"> 내용 입력 : </label>
+          <input
+            type="text"
+            id="text"
+            value={text}
+            placeholder=" 내용을 입력하세요."
+            onChange={(event) => setText(event.target.value)}
+          />
+        </div>
+        <div className={Styles.num_input}>
           <div>
-            <label htmlFor="text">텍스트:</label>
-            <input
-              type="text"
-              id="text"
-              value={text}
-              onChange={(event) => setText(event.target.value)}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="option1">선택지 1:</label>
+            <label htmlFor="option1">선택지 1 : </label>
             <input
               type="text"
               id="option1"
               value={options[0]}
+              placeholder=" 1번 정답"
               onChange={(event) => handleOptionChange(event, 0)}
             />
           </div>
           <div>
-            <label htmlFor="option2">선택지 2:</label>
+            <label htmlFor="option2">선택지 2 : </label>
             <input
               type="text"
               id="option2"
               value={options[1]}
+              placeholder=" 2번 정답"
               onChange={(event) => handleOptionChange(event, 1)}
             />
           </div>
           <div>
-            <label htmlFor="option3">선택지 3:</label>
+            <label htmlFor="option3">선택지 3 : </label>
             <input
               type="text"
               id="option3"
               value={options[2]}
+              placeholder=" 3번 정답"
               onChange={(event) => handleOptionChange(event, 2)}
             />
           </div>
           <div>
-            <label htmlFor="option4">선택지 4:</label>
+            <label htmlFor="option4">선택지 4 : </label>
             <input
               type="text"
               id="option4"
               value={options[3]}
+              placeholder=" 4번 정답"
               onChange={(event) => handleOptionChange(event, 3)}
             />
           </div>
-          <div>
-            <label htmlFor="answer">정답:</label>
-            <select
-              id="answer"
-              value={answer}
-              onChange={(event) => setAnswer(event.target.value)}
-            >
-              <option value={0}>선택지 1</option>
-              <option value={1}>선택지 2</option>
-              <option value={2}>선택지 3</option>
-              <option value={3}>선택지 4</option>
-            </select>
-          </div>
+        </div>
+        <div>
+          <label htmlFor="answer">정답 : </label>
+          <select
+            id="answer"
+            value={answer}
+            onChange={(event) => setAnswer(event.target.value)}
+          >
+            <option value={0}>선택지 1</option>
+            <option value={1}>선택지 2</option>
+            <option value={2}>선택지 3</option>
+            <option value={3}>선택지 4</option>
+          </select>
+        </div>
 
-          <div>
-            <label htmlFor="photo">사진:</label>
-            <input type="file" id="photo" onChange={handleFileChange} />
-          </div>
+        <div>
+          <label htmlFor="photo">사진 : </label>
+          <input type="file" id="photo" onChange={handleFileChange} />
+        </div>
 
-          <button type="submit">추가하기</button>
-        </form>
-        <Getquestion />
-      </div>
+        <button className={Styles.adm_btn} type="submit">
+          추가하기
+        </button>
+      </form>
+      <Getquestion />
     </>
   );
 };
